30410420a60fae7bd1cb09886ac5531b6ad65b34
its-micah <mlanier86@gmail.com>
2015-03-25 12:51:39 -0500

    Initial Commit

---
+++ PizzaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PizzaHunter
+//
+//  Created by Micah Lanier on 3/25/15.
+//  Copyright (c) 2015 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
